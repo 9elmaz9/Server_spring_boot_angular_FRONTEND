ec309ce87543172481381cd5b8aa0a0f448107a5
9elmaz9 <kakbyelmaz@gmail.com>
2024-06-19 15:51:02 +0200

    Initial commit

---
+++ src/app/interface/server.ts
@@ -0,0 +1,14 @@
+import { Status } from "../enum/status.enum";
+
+export interface Server {
+    filter(arg0: (server: any) => boolean): unknown;
+
+    id: number;
+    ipAddress: string;
+    name: string;
+    memory: string;
+    type: string;
+    imageUrl: string;
+    status: Status;
+
+}
